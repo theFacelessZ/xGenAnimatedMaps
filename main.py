2cefc5d33c8d373baf40a7d802b497ad078ab0df
Anthony Lupow <a.lupov@unlitsolutions.com>
2018-03-17 22:54:38 +0300

    Refactor ui elements.

--- main.py
+++ main.py
@@ -3,7 +3,110 @@ import maya.cmds as cmds
 import pymel.core as pm
 import xgenm as xg
 import os
-import shutil
+import unicodedata
+
+
+class Utils:
+
+    @staticmethod
+    def safe_string(value):
+        return unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+
+
+# Look, autodesk, this is how it must've actually worked from the box.
+# Amen.
+class UiElementWrapper(object):
+
+    def __init__(self, id, default_value='', change_callback=None, project=None):
+        self.id_pure = id
+        self.on_change = change_callback
+        self.value = ''
+        self.settings = None
+
+        if default_value:
+            self.set_value(default_value)
+
+        if project:
+            self.settings = project.settings
+
+    def set_value(self, value):
+        if type(value) is unicode:
+            value = Utils.safe_string(value)
+
+        self.value = value
+
+        if self.settings:
+            self.settings.set(self.id_pure, value)
+
+        if callable(self.on_change):
+            self.on_change()
+
+
+class UiOptionMenu(UiElementWrapper):
+
+    def __init__(self, id, value='', label='', change_callback=None, project=None):
+        super(UiOptionMenu, self).__init__(id, value, change_callback, project)
+
+        # Create the element itself.
+        self.id = pm.optionMenu(id, label=label, cc=self.set_value)
+
+    def get_items(self):
+        return pm.optionMenu(self.id, q=True, ill=True) or []
+
+    def set_items(self, items):
+        for item in self.get_items():
+            pm.deleteUI(item)
+
+        if not len(items):
+            return
+
+        for item in items:
+            pm.menuItem(item, label=item, parent=self.id)
+
+        # Update the element.
+        self.set_value(items[0])
+
+
+class UiObjectSelection(UiElementWrapper):
+
+    def __init__(self, id, object_type='', label='', button_label='Selection', change_callback=None, project=None):
+        super(UiObjectSelection, self).__init__(id, change_callback=change_callback, project=project)
+        self.object_type = object_type
+
+        # Create the element itself.
+        self.id = pm.textFieldButtonGrp(id, label=label, bl=button_label, bc=self.object_selection, tcc=self.set_value)
+
+    def object_selection(self):
+        obj = cmds.ls(sl=True)
+
+        # Do nothing in case no objects are present in the selection.
+        if not len(obj):
+            return cmds.warning('The selection is empty.')
+
+        # Otherwise use the first reference.
+        obj = obj[0]
+
+        # Check the object type compliance, if any is given.
+        if self.object_type and not cmds.objectType(obj) == self.object_type:
+            return cmds.warning('Selected object is of a wrong type. Anticipated %s.' % self.object_type)
+
+        self.set_value(obj)
+
+    def set_value(self, value):
+        super(UiObjectSelection, self).set_value(value)
+
+        # Update textfield value.
+        if not self.value == value:
+            pm.textFieldButtonGrp(self.id, e=True, tx=value)
+
+
+class UiTextField(UiElementWrapper):
+
+    def __init__(self, id, label='', default_value='', project=None):
+        super(UiTextField, self).__init__(id, default_value=default_value, project=project)
+
+        # Create the element itself.
+        pm.textFieldGrp(id, label=label, tx=default_value, tcc=self.set_value)
 
 
 class ProjectSettings:
@@ -79,79 +182,6 @@ class PtxBaker(XgenAnimSettingsDependant):
                 pass
 
 
-# TODO: Implement ui elements factory.
-class UiFactory:
-
-    @staticmethod
-    def field_change_callback(project, id, value):
-        project.settings.set(id, value)
-
-    @staticmethod
-    def text_field(project, id, label='', default_value=''):
-        return pm.textFieldGrp(id, label=label, tx=default_value, tcc=partial(UiFactory.field_change_callback, project, id))
-
-    @staticmethod
-    def range_field(project, id, label='', default_value=0):
-        return pm.rangeControl(id, ann=label, min=0, max=1000, cc=partial(UiFactory.field_change_callback, project, id))
-
-    @staticmethod
-    def object_selection_callback(group_id, object_type=None):
-        obj = cmds.ls(sl=True)
-
-        # Do nothing in case no objects are present in the selection.
-        if not len(obj):
-            return cmds.warning('Selection is empty.')
-
-        # Otherwise use the first reference.
-        obj = obj[0]
-
-        # Check the object type compliance, if any is given.
-        if object_type and not cmds.objectType(obj) == object_type:
-            return cmds.warning('Selected object is of a wrong type. Anticipated %s.' % object_type)
-
-        pm.textFieldButtonGrp(group_id, e=True, tx=obj)
-
-    @staticmethod
-    def object_selection(project, id, label='', object_type=None, default_value=''):
-        return pm.textFieldButtonGrp(id, label=label, tx=default_value, bl='Selection',
-                              bc=partial(UiFactory.object_selection_callback, id, object_type),
-                              tcc=partial(UiFactory.field_change_callback, project, id))
-
-    @staticmethod
-    def option_selection(self, id, label='', items_callback=None, change_callback=None, edit=False):
-        items = []
-        label = label or pm.optionMenu(id, q=True, l=True)
-        change_callback = change_callback or ''
-
-        if items_callback:
-            items = items_callback()
-
-        result = pm.optionMenu(id, label=label, cc=change_callback, edit=edit)
-
-        for item in pm.optionMenu(result, q=True, ill=True) or []:
-            pm.deleteUI(item)
-
-        for item in items:
-            pm.menuItem(item, label=item, parent=result)
-
-        if change_callback and len(items):
-            change_callback()
-
-        return result
-
-    @staticmethod
-    def directory_selection_callback(group_id):
-        directory = cmds.fileDialog2(fm=3)
-
-        if len(directory):
-            pm.textFieldButtonGrp(group_id, e=True, tx=directory[0])
-
-    def directory_selection(self, id, label='', default_value=''):
-        return pm.textFieldButtonGrp(id, label=label, tx=default_value, bl='Browse',
-                              bc=partial(self.directory_selection_callback, id),
-                              tcc=partial(self.field_change_callback, self, id))
-
-
 class XgenAnim:
 
     ui_id = 'xgenanim'
@@ -169,33 +199,65 @@ class XgenAnim:
         # Form the UI.
         with pm.columnLayout():
             with pm.frameLayout(l='Map Properties', cll=False, cl=False):
-                UiFactory.option_selection(self, 'xgenCollection', label='Collection',
-                                           items_callback=xg.palettes,
-                                           change_callback=self.update_collections)
-                UiFactory.option_selection(self, 'xgenDescription', label='Description')
-                # Make sure descriptions records are up to date.
-                self.update_descriptions()
+                self.ui_collection = UiOptionMenu('xgenCollection', label='Collection',
+                                                  change_callback=self.update_descriptions, project=self)
+                self.ui_description = UiOptionMenu('xgenDescription', label='Description',
+                                                   change_callback=self.update_objects, project=self)
+                self.ui_objects = UiOptionMenu('xgenObject', label='Object',
+                                               change_callback=self.update_attributes, project=self)
+                self.ui_attributes = UiOptionMenu('xgenAttribute', label='Attribute',
+                                                  project=self)
 
-                UiFactory.object_selection(self, 'xgenSequence', label='Animated Sequence Node', object_type='file')
-                UiFactory.object_selection(self, 'xgenEmitter', label='xGen Emitter Object', object_type='transform')
-                UiFactory.text_field(self, 'xgenMap', label='Result map path', default_value='default/map')
-                UiFactory.range_field(self, 'xgenResolution', label='Result map resolution', default_value=100)
+                # Set the collection items to trigger the rest.
+                self.update_collections()
+
+                pm.button('update', label='Update', c=self.update_collections)
+
+                self.ui_sequence = UiObjectSelection('xgenSequence', label='Animated Sequence Node', object_type='file', project=self)
+                self.ui_emitter = UiObjectSelection('xgenEmitter', label='xGen Emitter Object', object_type='transform', project=self)
+                self.ui_map = UiTextField('xgenMap', label='Result map path', default_value='default/map', project=self)
+
                 pm.button('assign', label='Assign', c=self.assign)
-                pm.button('update', label='Update', c=self.update_ui)
 
         self.window.show()
 
-    def update_ui(self, flag):
-        self.update_collections()
-        self.update_descriptions()
+    def get_collection(self):
+        return self.settings.get('xgenCollection', '')
+
+    def get_description(self):
+        return self.settings.get('xgenDescription', '')
+
+    def get_object(self):
+        return self.settings.get('xgenObject', '')
+
+    def update_collections(self, flag=False):
+        if not self.ui_collection:
+            return
+
+        self.ui_collection.set_items(xg.palettes())
 
     def update_descriptions(self):
-        UiFactory.option_selection(self, 'xgenDescription', edit=True,
-                                   items_callback=lambda: xg.descriptions(self.settings.get('xgenCollection', '')))
+        if not self.ui_description:
+            return
 
-    def update_collections(self):
-        UiFactory.option_selection(self, 'xgenCollection', edit=True,
-                                   items_callback=lambda: xg.palettes())
+        collection = self.get_collection()
+        descriptions = xg.descriptions(collection)
+
+        self.ui_description.set_items(descriptions)
+
+    def update_objects(self):
+        if not self.ui_objects:
+            return
+
+        objects = xg.objects(self.get_collection(), self.get_description(), True)
+        self.ui_objects.set_items(objects)
+
+    def update_attributes(self):
+        if not self.ui_attributes:
+            return
+
+        attributes = xg.allAttrs(self.get_collection(), self.get_description(), self.get_object())
+        self.ui_attributes.set_items(attributes)
 
     def assign(self, flag):
         PtxBaker.perform_conversion(self)
